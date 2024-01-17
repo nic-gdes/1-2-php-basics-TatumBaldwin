7cac8a8374d8093cca26c1c385ecbd68063ca375
Tatum Baldwin <tjbaldwin@students.nic.edu>
2024-01-17 15:01:34 -0800

    created one variable of each datatypes

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//create a variable string
+$string = "this is a string";
+//create a variable integer
+$integer = 6;
+//create a variable float
+$float = 0.4;
+//create a variable boolean
+$boolean = true;
+?>
